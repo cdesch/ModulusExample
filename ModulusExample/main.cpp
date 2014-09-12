9e3a71b7cd95378a0d6e391e83f8be756e0707a1
Chris <cdesch@gmail.com>
2014-09-12 19:32:31 -0400

    Modulus Example
    Signed-off-by: Chris <cdesch@gmail.com>

--- ModulusExample/main.cpp
+++ ModulusExample/main.cpp
@@ -8,11 +8,37 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    
+    long x =  98765432;
+    
+    
+    cout << "Mod 10 " << x % 10 << endl;
+    cout << "Mod 100 " << x % 100 << endl;
+    cout << "Mod 1000 " << x % 1000 << endl;
+    cout << "Mod 10000 " << x % 10000 << endl;
+    cout << "Mod 100000 " << x % 100000 << endl;
+    cout << "Mod 1000000 " << x % 1000000 << endl;
+    
+    cout << "Div 10 " << x / 10 << endl;
+    cout << "Div 100 " << x / 100 << endl;
+    cout << "Div 1000 " << x / 1000 << endl;
+    cout << "Div 10000 " << x / 10000 << endl;
+    cout << "Div 100000 " << x / 100000 << endl;
+    cout << "Div 1000000 " << x / 1000000 << endl;
+
+    cout << "Year  " << x / 10000 << endl;
+    cout << "Month  " << (x / 100) % 100 << endl;
+    cout << "Day  " << x % 100 << endl;
+    
+
     return 0;
 }
 
